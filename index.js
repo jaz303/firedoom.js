21b827f0eb2410738448521b0a46f8c638c16131
Jason Frame <jason@onehackoranother.com>
2014-04-19 23:50:08 +0100

    file change events being relayed to browser

--- index.js
+++ index.js
@@ -7,8 +7,55 @@
 // 	World 			: require('./lib/World')
 // };
 
-var watcher = require('./server/watcher');
+// var watcher = require('./server/watcher');
 
-console.log("watching...");
+//
+// serving
 
-watcher(__dirname + '/example');
+var Server = require('ws').Server;
+var wss = new Server({port: 8080, path: "/watcher"});
+
+wss.broadcast = function(data) {
+	for (var i in this.clients) {
+		this.clients[i].send(data);
+	}
+}
+
+wss.on('connection', function(socket) {
+	console.log("client connected");
+});
+
+//
+// watching
+
+var watchr = require('watchr');
+
+watchr.watch({
+    path        : __dirname + '/example',
+    persistent  : true,
+    listeners   : {
+        change: function(type, path, currStat, oldStat) {
+
+        	var changeEvent = {
+        		type: "fs.modified",
+        		fsType: type,
+        		path: path,
+        		currStat: currStat,
+        		oldStat: oldStat
+        	};
+
+        	var msg = new Buffer(JSON.stringify(changeEvent), 'utf8');
+
+        	wss.broadcast(msg);
+
+        	// wss.broadcast(JSON.stringify({
+        	// 	type: "fs.modified",
+        	// 	fsType: type,
+        	// 	path: path,
+        	// 	currStat: currStat,
+        	// 	oldState: oldStat
+        	// }));
+
+        }
+    }
+});
